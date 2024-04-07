4971cb5c6a4368030cff2d66dd692fd428064879
Jack <wx_db13ec03ee04486190928962ab7a34eb@git.code.tencent.com>
2024-04-07 21:09:40 +0800

    add: api index

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,7 +1,34 @@
 import { Handler } from "@/types/Handler.ts";
+import { getAppsRandom, getApp, getAppsByCategory } from "@/lib/db.ts";
 
 const region = Deno.env.get("DENO_REGION") || "localhost";
 
-export const handler: Handler = () => {
-	return new Response("Hello World from " + region + "!\n");
+export const handler: Handler = async (req) => {
+	const url = new URL(req.url);
+	const method = url.searchParams.get("method");
+	if(method == 'getApp'){
+		let id = url.searchParams.get("id");
+		console.log(id);
+		console.log(method);
+		const app = await getApp(id? id:"");
+		console.log(app);
+		const otherApps = await getAppsRandom(5, app?.id);
+		return new Response(JSON.stringify({app: app, otherApps: otherApps}),{
+			headers: {"Access-Control-Allow-Origin": "*"}
+		})
+	}	
+	if(method == 'getAppsByCategory'){
+		let category = url.searchParams.get("category") || '0';
+		let apps = []
+		if(category == '0'){
+			apps = await getAppsRandom(8);
+		} else {
+			apps = await getAppsByCategory(50, category);
+		}
+		return new Response(JSON.stringify(apps),{
+			headers: {"Access-Control-Allow-Origin": "*"}
+		})
+	}
+	return new Response("Hello World from " + region + "!\n") 
+
 };
